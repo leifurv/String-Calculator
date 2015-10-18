3d6cbd7d8e6bf0a88aba81d1a5ddfa09a75b00fe
Leifur Viktor Guðmundsson <leifurg14@i-7f3b5dd9.csd07cloud.internal>
2015-10-18 21:28:02 +0000

    step2

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+package TDD;
+
+public class stringCalculator {
+	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
+		else
+			return 1;
+	} 
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
+	}
+     
+    private static int sum(String[] numbers){
+    	int total = 0;
+		for(String number : numbers){
+			if (!number.trim().isEmpty()) {
+			total += toInt(number);
+			}
+		}
+		return total;
+    }
+}
