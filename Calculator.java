ba6e60d18e9637ee9fb3ab6d9b891c3f07c244e1
Leifur Viktor Guðmundsson <leifurg14@i-7f3b5dd9.csd07cloud.internal>
2015-10-18 22:21:31 +0000

    step4

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,33 @@
 package TDD;
 
 public class stringCalculator {
-	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		}
-		else
-			return 1;
-	} 
+    	public static int add(String text){
+            	String delimeter = "!|n";
+            	String numWoutDelimeter = text;
+            	if(text.startsWith("//")){
+                    	int Index = text.indexOf("//") + 2;
+                    	delimeter = text.substring(Index, Index + 1);
+                    	numWoutDelimeter = text.substring(text.indexOf("n") + 1);
+            	}
+            	return sum(numWoutDelimeter, delimeter);
+    	}
 
-	private static int toInt(String number){
-		return Integer.parseInt(number);
+    	private static int toInt(String number){
+        	return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-		return numbers.split(",|n");
+	private static String[] splitNumbers(String numbers, String delimeter){
+        	return numbers.split(delimeter);
 	}
-     
-    private static int sum(String[] numbers){
-    	int total = 0;
-		for(String number : numbers){
-			if (!number.trim().isEmpty()) {
-			total += toInt(number.trim());
-			}
-		}
-		return total;
-    }
+
+	private static int sum(String numbers, String delimeter){
+        	int total = 0;
+        	String[] arr = numbers.split(delimeter);
+        	for(String number : arr){
+                	if (!number.trim().isEmpty()) {
+                	total += toInt(number.trim());
+                	}
+        	}
+        	return total;
+	}
 }
